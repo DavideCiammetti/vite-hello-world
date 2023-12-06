f6ae4db3a3a62ea24c9f8dd87f4df46c60fe8585
DavideCiammetti <davideciammetti97@gmail.com>
2023-12-06 17:39:11 +0100

    fix: creare una seconda app-AppHero e collegarla

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <script>
+import AppHero from './AppHero.vue';
 export default{
   name:'appComponent',
     data(){
       return{
-
+        'title': 'La mia prima app con Vite!',
       };
     },
+    components:{
+      AppHero,
+    }
 };
 </script>
 
 <template>
-  <h1>La mia prima app con Vite!</h1>
+  <h1>{{ title }}</h1>
+  <AppHero/>
 </template>
 
 <style scoped>
---
+++ src/AppHero.vue
@@ -0,0 +1,19 @@
+<script>
+export default{
+  name:'appComponentHero',
+    data(){
+      return{
+        'title': 'app hero con vite',
+        'text': 'nuova app con vue',
+      };
+    },
+};
+</script>
+
+<template>
+  <h3>{{ title }}</h3>
+  <p>{{ text }}</p>
+</template>
+
+<style scoped>
+</style>
